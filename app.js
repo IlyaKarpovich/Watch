f77d39e1124718c4c2cdce981b18668809da4a5e
IlyaKarpovich <98961471+IlyaKarpovich@users.noreply.github.com>
2022-05-25 13:28:44 +0300

    v.1

---
+++ app.js
@@ -0,0 +1,17 @@
+{
+    status: "ok",
+    totalResults: 12211,
+    articles: [
+    {
+    source: {
+    id: "reuters",
+    name: "Reuters",
+    }
+    author: null,
+    title: "Breakingviews - Snap! Elon Musk has another Twitter excuse - Reuters",
+    description: "Snap <a href=\"https://www.reuters.com/companies/SNAP.N\" target=\"_blank\">(SNAP.N)</a> could be Twitter’s <a href=\"https://www.reuters.com/companies/TWTR.N\" target=\"_blank\">(TWTR.N)</a> raven. The disappearing-message app <a href=\"https://d18rn0p25nwr6d.cloudfr…",
+    url: "https://www.reuters.com/breakingviews/snap-elon-musk-has-another-twitter-excuse-2022-05-24/",
+    urlToImage: "https://www.reuters.com/resizer/5WcBP1FYM5BW38-OQqzAWneJhsg=/1200x628/smart/filters:quality(80)/cloudfront-us-east-2.images.arcpublishing.com/reuters/YFCZ5XDGXBAFHCDN7XLSNQCN6A.JPG",
+    publishedAt: "2022-05-24T16:10:00Z",
+    content: "NEW YORK, May 24 (Reuters Breakingviews) - Snap (SNAP.N) could be Twitters (TWTR.N) raven. The disappearing-message app abruptly slashed its second-quarter forecasts, sending its shares spiraling som… [+2834 chars]"
+    }
---
+++ js/script.js
@@ -0,0 +1,112 @@
+// // Calendar
+function calendar() {
+const lang = 'en-US';
+let date = new Date();
+
+let dayNumber = date.getDate();
+let dayName = date.toLocaleString(lang,{weekday: 'long'});
+let month = date.getMonth();
+let monthName = date.toLocaleString(lang,{month: 'long'});
+let year = date.getFullYear();
+
+document.getElementById('month').innerHTML = monthName;
+document.getElementById('dayName').innerHTML = dayName;
+document.getElementById('dayNumber').innerHTML = dayNumber;
+document.getElementById('year').innerHTML = year;
+}
+calendar();
+//Watch
+function clock() {
+    const hoursArrow = document.querySelector(".hours");
+    const minutesArrow = document.querySelector(".minutes");
+    const secondsArrow = document.querySelector(".seconds");
+    const deg = 6;
+
+    setInterval(() => {
+        const day = new Date();
+        const hours = day.getHours() * 30;
+        const minutes = day.getMinutes() * deg;
+        const seconds = day.getSeconds() * deg;
+
+        hoursArrow.style.transform = `rotateZ(${hours + (minutes / 12)}deg)`;
+        minutesArrow.style.transform = `rotateZ(${minutes}deg)`;
+        secondsArrow.style.transform = `rotateZ(${seconds}deg)`;
+    }, 0)
+    
+}
+clock();
+// Цифров. часы
+const timeElement = document.querySelector('.time');
+
+const tick = () => {
+    const now = new Date();
+    const hours = now.getHours();
+    const min = now.getMinutes();
+    const sec = now.getSeconds();
+    const colon = sec % 2 === 0 ? ':' : ' ';
+    timeElement.textContent = `TIME: ${hours}${colon}${min}${colon}${sec}`;
+    
+}
+
+tick();
+
+setInterval(tick, 100);
+//Cмена твитов
+function changeTwit() {
+    let 
+}
+//Смена блоков меню
+function ShowWatch() {
+    let service = document.querySelector('.service');
+    let about = document.querySelector('.about');
+    let contact = document.querySelector('.contact');
+    let watch = document.querySelector('.watch')
+    service.classList = "service animate__animated animate__backOutUp";
+    about.classList = "about animate__animated animate__backOutUp";
+    contact.classList = "contact animate__animated animate__backOutUp";
+    watch.classList = "watch animate__animated animate__slideInUp"
+    console.log(about);
+}
+function textShowService() {
+    let service = document.querySelector('.service');
+    let about = document.querySelector('.about');
+    let contact = document.querySelector('.contact');
+    let watch = document.querySelector('.watch')
+    service.classList = "service animate__animated animate__slideInUp";
+    service.style.display = "block";
+    about.classList = "about animate__animated animate__backOutUp";
+    contact.classList = "contact animate__animated animate__backOutUp";
+    watch.classList = " watch animate__animated animate__backOutUp";
+    console.log(about);
+}
+function textShowAbout() {
+    let service = document.querySelector('.service');
+    let about = document.querySelector('.about');
+    let contact = document.querySelector('.contact');
+    let watch = document.querySelector('.watch')
+    service.classList = "service animate__animated animate__backOutUp";
+    about.classList = "about animate__animated animate__slideInUp";
+    about.style.display = "block";
+    contact.classList = "contact animate__animated animate__backOutUp";
+    watch.classList = " watch animate__animated animate__backOutUp";
+    console.log(about);
+}
+function textShowContact() {
+    let service = document.querySelector('.service');
+    let about = document.querySelector('.about');
+    let contact = document.querySelector('.contact');
+    let watch = document.querySelector('.watch')
+    service.classList = "service animate__animated animate__backOutUp";
+    about.classList = "about animate__animated animate__backOutUp";
+    contact.classList = "contact animate__animated animate__slideInUp";
+    contact.style.display = "block";
+    watch.classList = " watch animate__animated animate__backOutUp";
+}
+
+
+
+
+
+
+
+
